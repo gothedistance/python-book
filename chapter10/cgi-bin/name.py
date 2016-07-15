2be65b6ce6ba34e3b47a8495dec0e2ba62a532fc
YUMOTO Michitaka <yumoto.michitaka@gmail.com>
2016-07-15 12:19:14 +0900

    name.py modified

---
+++ chapter10/cgi-bin/name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import cgi
+
+html_body = """
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<p>あなたのお名前は <span style="font-size:48px"> %s </span>さんです！</p>
+</body></html>
+"""
+
+form = cgi.FieldStorage()
+
+print("Content-type: text/html")
+print(html_body % form['name'].value)
